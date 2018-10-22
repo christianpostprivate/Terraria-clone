6134df4e900b473296941aad83dfa4010b6f13d3
MattR0se <37417239+MattR0se@users.noreply.github.com>
2018-10-22 14:50:29 +0200

    Add files via upload

--- terraria_like.py
+++ terraria_like.py
@@ -1,20 +1,29 @@
+'''
+Simple engine for a Terraria-style game
+
+'''
+
 import pygame as pg
 import traceback
 from random import random, choice
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_SCALE = 1
+# some constants
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 300
+SCREEN_SCALE = 2
 
-TILESIZE = 20
+TILESIZE = 16
 TILESIZE_SQUARED = TILESIZE ** 2
-MAP_WIDTH = 80 * TILESIZE
-MAP_HEIGHT = 60 * TILESIZE
-
+MAP_WIDTH = 340 * TILESIZE
+MAP_HEIGHT = 80 * TILESIZE
 GUI_HEIGHT = 3 * TILESIZE
 
 vec = pg.math.Vector2
 
+GRAVITY = vec(0, 0.5)
+NULL = vec(0, 0)
+
+# colors (no way)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 DARKGREY = (60, 60, 60)
@@ -28,9 +37,9 @@ SKYBLUE = (200, 200, 255)
 LIGHTBLUE = (180, 180, 255)
 BROWN = (100, 60, 20)
 YELLOW = (250, 200, 100)
+SILVER = (92, 92, 130)
 
-GRAVITY = 0.5
-
+# dict for the different blocks and their values
 BLOCK_TYPES = {
         'grass': {
                 'color': DARKGREEN,
@@ -56,13 +65,23 @@ BLOCK_TYPES = {
                 'color': RUBY,
                 'hardness': 5,
                 'weight': 1
-                }
+                },
+        'ore': {
+                'color': SILVER,
+                'hardness': 3,
+                'weight': 1
+                },       
         }
 
 
 
 def collide(sprite, group, dir_):
+    '''
+    collision function
+    https://github.com/kidscancode/pygame_tutorials
+    '''
     if dir_ == 'x':
+        # horizontal collision
         hits = pg.sprite.spritecollide(sprite, group, False)
         if hits:
             if hits[0] == sprite:
@@ -72,13 +91,14 @@ def collide(sprite, group, dir_):
                 sprite.pos.x = hits[0].rect.left - sprite.rect.w
             # hit from right
             elif hits[0].rect.right < sprite.rect.right:
-                sprite.pos.x = hits[0].rect.right# + sprite.rect.w
+                sprite.pos.x = hits[0].rect.right
                             
             sprite.vel.x = 0
             sprite.rect.left = sprite.pos.x
             return True
             
     elif dir_ == 'y':
+        # vertical collision
         hits = pg.sprite.spritecollide(sprite, group, False)
         if hits:
             if hits[0] == sprite:
@@ -88,7 +108,7 @@ def collide(sprite, group, dir_):
                 sprite.pos.y = hits[0].rect.top - sprite.rect.h
             # hit from bottom
             elif hits[0].rect.top < sprite.rect.top:
-                sprite.pos.y = hits[0].rect.bottom #+ sprite.rect.h
+                sprite.pos.y = hits[0].rect.bottom
                 
             sprite.vel.y = 0
             sprite.rect.top = sprite.pos.y
@@ -97,17 +117,53 @@ def collide(sprite, group, dir_):
 
 
 
-class Player(pg.sprite.Sprite):
-    def __init__(self, game, x, y):
+class Physics_object(pg.sprite.Sprite):
+    '''
+    parent sprite for all sprites that experience physics
+    '''
+    def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.game = game
-        self.pos = vec(x, y)
+        self.pos = vec(0, 0)
         self.acc = vec(0, 0)
         self.vel = vec(0, 0)
-        self.image = pg.Surface((20, 30), flags=pg.SRCALPHA)
-        self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.rect.topleft = self.pos
+        
+        self.speed = 0.5
+        self.friction = -0.12
+        self.gravity = vec(GRAVITY)
+        
+        
+    def update(self, others):        
+        self.acc = self.gravity
+        
+        self.vel += self.acc
+        pos_change = self.vel + 0.5 * self.acc
+        if pos_change.length_squared() >= TILESIZE_SQUARED:
+            pos_change.scale_to_length(TILESIZE_SQUARED - 1)
+        self.pos += pos_change
+        # collision detection
+        self.rect.left = self.pos.x
+        collide(self, others, 'x')
+        self.rect.top = self.pos.y
+        collide(self, others, 'y')
+
+        # kill if not in bounds
+        if not -TILESIZE < self.pos.x < MAP_WIDTH + TILESIZE:
+            self.kill()
+        if not - 5 * TILESIZE < self.pos.y < MAP_HEIGHT + TILESIZE:
+            self.kill()
+            
+
+
+class Player(Physics_object):
+    def __init__(self, game, x, y):
+        self.game = game
+        self.image = pg.Surface((TILESIZE, int(TILESIZE * 1.5)), flags=pg.SRCALPHA)
+        self.image.fill(RED)
+        super().__init__()
+        self.game.all_sprites.add(self)
+        self.pos = vec(x, y)
         
         self.speed = 0.5
         self.friction = -0.12
@@ -116,10 +172,10 @@ class Player(pg.sprite.Sprite):
         self.inventory_types = []
         self.inventory_selected = 0
         
+        self.reach = 60
         
-    def update(self, blocks):        
-        self.acc = vec(0, GRAVITY)
         
+    def update(self):
         pressed = pg.key.get_pressed()
         move_left = pressed[pg.K_a]
         move_right = pressed[pg.K_d]
@@ -128,18 +184,8 @@ class Player(pg.sprite.Sprite):
         self.acc.x *= self.speed       
         self.acc.x += self.vel.x * self.friction
         
-        self.vel += self.acc  
-        self.pos += self.vel + 0.5 * self.acc
-        
-        self.rect.left = self.pos.x
-        collide(self, blocks, 'x')
-        self.rect.top = self.pos.y
-        collide(self, blocks, 'y')
-        
-        self.pos.x = max(0, min(self.pos.x, MAP_WIDTH - self.rect.w))
-        self.pos.y = max(0, min(self.pos.y, MAP_HEIGHT - self.rect.h))        
-        self.rect.topleft = self.pos
-        
+        super().update(self.game.blocks)
+       
         # Add and remove blocks
         if self.game.mouseclickedleft:
             if len(self.inventory_types) > 0:
@@ -153,15 +199,12 @@ class Player(pg.sprite.Sprite):
                 if block.hardness == 0:
                     self.game.grid.player_remove_at(self.game.block_pos)
                     block.kill()
-                    
-        
+                          
         for key in self.inventory:
             self.inventory[key] = min(self.inventory[key], 999)            
-        
-        
+             
         # inventory stuff
-        self.inventory_types = list(self.inventory)
-        
+        self.inventory_types = list(self.inventory)       
         
         
     def jump(self, blocks):
@@ -183,6 +226,7 @@ class Block(pg.sprite.Sprite):
         self.game = game
         self.grid = self.game.grid
         self.type = type_
+        self.game.all_sprites.add(self)
         self.game.blocks.add(self)
         self.pos = vec(x, y)
         self.image = pg.Surface((TILESIZE, TILESIZE))
@@ -192,7 +236,7 @@ class Block(pg.sprite.Sprite):
         
         self.acc = vec(0, 0)
         self.vel = vec(0, 0)
-        self.friction = -0.1
+        self.gravity = vec(GRAVITY)
         
         self.hardness = BLOCK_TYPES[self.type]['hardness']
         self.weight = BLOCK_TYPES[self.type]['weight']
@@ -208,8 +252,8 @@ class Block(pg.sprite.Sprite):
     def update(self):
         if self.type == 'dirt':
             # check if nothing is above on the map
-            if (not self.grid.get_at(vec(self.pos.x, self.pos.y - TILESIZE))
-                and not self.grid.get_at(vec(self.pos.x, 
+            if (not self.grid.get_at((self.pos.x, self.pos.y - TILESIZE))
+                and not self.grid.get_at((self.pos.x, 
                                              self.pos.y + TILESIZE)) == None):
                 self.age += 1
                 if self.age >= 600:
@@ -218,11 +262,12 @@ class Block(pg.sprite.Sprite):
                     self.age = 0
         
         if self.type == 'sand':
-            if (self.grid.get_at(vec(self.pos.x, self.pos.y + TILESIZE))
+            # sands experiences gravity if no block is below it
+            if (self.grid.get_at((self.pos.x, self.pos.y + TILESIZE))
                 and self.state == 'STATIC'):
                 return
             else:
-                self.acc = vec(0, GRAVITY) * self.weight
+                self.acc = self.gravity * self.weight
                 
                 self.vel += self.acc  
                 if self.vel.length_squared() >= TILESIZE_SQUARED:
@@ -245,10 +290,73 @@ class Block(pg.sprite.Sprite):
                 if self.state == 'STATIC':
                     if not self.game.grid.get_at(self.pos):
                         self.game.grid.block_add(self.pos, self)
-                        
+    
+    
+    
+class Block_drop(Physics_object):
+    def __init__(self, game, type_, pos):
+        self.game = game
+        self.type = type_
+        self.image = pg.Surface((10, 10))
+        self.image.fill(BLOCK_TYPES[self.type]['color'])
+        super().__init__()
+        self.game.all_sprites.add(self)
+        self.game.drops.add(self)        
+        self.pos = vec(pos)
+        self.rect.topleft = self.pos
+
+    
+    def update(self):
+        # if not in camera range, kill
+        rect = pg.Rect((0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT))
+        rect.topleft = self.game.camera.apply_point(rect.topleft)
+        if not self.rect.colliderect(rect):
+            self.kill()
+            return
+        
+        # apply physics
+        
+        blocks = self.game.blocks
+        super().update(blocks)
+        
+        player = self.game.player
+        
+        # object is drawn towards player
+        self_to_player = player.pos - self.pos  
+        if self_to_player.length_squared() < TILESIZE_SQUARED * 3:
+            self.gravity = NULL
+            self.pos += self_to_player.normalize() * 2     
+        
+        # check for collision with player
+        if pg.sprite.collide_rect(self, player):
+            self.kill()
+            if self.type in player.inventory:
+                player.inventory[self.type] += 1
+            else:
+                player.inventory[self.type] = 1
+        
+        self.seperate()
+        
+    
+    def seperate(self):
+        # seperate the block drops if they fall onto each other
+        if len(self.game.drops) <= 1:
+            return
+        for drop in self.game.drops:
+            if drop != self:
+                dist = self.pos - drop.pos
+                
+        if dist.length_squared() < 25:
+            # if they are less than 5 pixels apart, change their position
+            self.pos += dist.normalize()      
+            
             
          
 class Grid:
+    '''
+    object that generates and holds a 2D array of Block objects 
+    and also has methods for placing and removing them
+    '''
     def __init__(self, game, width, height):
         self.game = game
         self.width = width
@@ -261,6 +369,8 @@ class Grid:
         
         self.treasure_limit = 5
         
+        self.horizon = 20
+        
     
     def generate(self):
         '''
@@ -271,11 +381,11 @@ class Grid:
         '''
         placement_chance = 0.38
         
-        for y in range(self.height - 1):
+        # generates a grid with values True or False, than later fills
+        # it with blocks
+        for y in range(self.horizon, self.height - 1):
             for x in range(self.width):
                 if random() < placement_chance:
-                    #pos_x = x * TILESIZE
-                    #pos_y = y * TILESIZE
                     self.map[y][x] = True
                     
         for i in range(self.no_of_steps):
@@ -284,12 +394,15 @@ class Grid:
         # place block or None
         for y in range(self.height - 1):
             for x in range(self.width):
-                if self.map[y][x]:
-                    pos_x = x * TILESIZE
-                    pos_y = y * TILESIZE
-                    self.map[y][x] = Block(self.game, 'dirt', pos_x, pos_y)
-                else:
+                if y < self.horizon:
                     self.map[y][x] = None
+                else:
+                    if self.map[y][x]:
+                        pos_x = x * TILESIZE
+                        pos_y = y * TILESIZE
+                        self.map[y][x] = Block(self.game, 'dirt', pos_x, pos_y)
+                    else:
+                        self.map[y][x] = None
     
     
     def simulation_step(self, old_map):
@@ -325,11 +438,13 @@ class Grid:
                       neighbor_x >= self.width or neighbor_y >= self.height):
                     count += 1
                 elif map_[neighbor_y][neighbor_x]:
-                    count += 1
-        
+                    count += 1    
         return count
     
+    
     def place_grass(self):
+        # place grass block if there is a block below and 
+        # no block above
         for y in range(1, self.height - 1):
             for x in range(1, self.width - 1):
                 if (self.map[y][x] != None 
@@ -349,7 +464,7 @@ class Grid:
                     if nbs >= self.treasure_limit:
                         pos_x = x * TILESIZE
                         pos_y = y * TILESIZE
-                        self.map[y][x] = Block(self.game, 'ruby', pos_x, pos_y)
+                        self.map[y][x] = Block(self.game, 'ore', pos_x, pos_y)
                     
     
     def add(self, pos, type_):
@@ -398,17 +513,15 @@ class Grid:
         grid_y = int(pos.y // TILESIZE)
         block = self.map[grid_y][grid_x]
         if block:
-            inv = game.player.inventory
-            if block.type in inv:
-                inv[block.type] += 1
-            else:
-                inv[block.type] = 1
+            Block_drop(block.game, block.type, (block.rect.centerx - 5
+                                                + random() - 0.5,
+                                                block.rect.centery))
             self.map[grid_y][grid_x] = None
             
     
     def get_at(self, pos):
-        grid_x = int(pos.x // TILESIZE)
-        grid_y = int(pos.y // TILESIZE)
+        grid_x = int(pos[0] // TILESIZE)
+        grid_y = int(pos[1] // TILESIZE)
         return self.map[grid_y][grid_x]
     
 
@@ -495,8 +608,10 @@ class Game:
         self.mouseclickedleft = False
         self.mouseclickedright = False
         self.block_pos = vec(0, 0)
-         
+        
+        self.all_sprites = pg.sprite.Group()
         self.blocks = pg.sprite.Group()
+        self.drops = pg.sprite.Group()
         
         self.tiles_w = MAP_WIDTH // TILESIZE
         self.tiles_h = MAP_HEIGHT // TILESIZE
@@ -509,13 +624,6 @@ class Game:
         for i in range(self.tiles_w):
             pos = vec(i * TILESIZE, MAP_HEIGHT - TILESIZE)
             self.grid.set(pos, 'stone')
-            '''
-            pos = vec(i * TILESIZE, MAP_HEIGHT - TILESIZE * 2)
-            self.grid.add(pos, 'dirt')
-            pos = vec(i * TILESIZE, MAP_HEIGHT - TILESIZE * 3)
-            self.grid.add(pos, 'dirt')
-            pos = vec(i * TILESIZE, MAP_HEIGHT - TILESIZE * 4)
-            self.grid.add(pos, 'grass')'''
             
         self.background = pg.Surface((MAP_WIDTH, MAP_HEIGHT))
         self.background_rect = self.background.get_rect()
@@ -526,17 +634,7 @@ class Game:
         self.player = Player(self, spawn_pos.x, spawn_pos.y)
         self.gui = GUI(self)
         self.camera = Camera(MAP_WIDTH, MAP_HEIGHT)
-        
-        
-        '''
-        for i in range(self.tiles_h):
-            pg.draw.line(self.background, LIGHTBLUE, (0, i * TILESIZE), 
-                         (MAP_WIDTH, i * TILESIZE))
-        for i in range(self.tiles_w):
-            pg.draw.line(self.background, LIGHTBLUE, (i * TILESIZE, 0), 
-                         (i * TILESIZE, MAP_HEIGHT))'''
-            
-            
+           
         
         # ------- debugging!
         #self.player.inventory['dirt'] = 999
@@ -553,25 +651,41 @@ class Game:
                         and self.grid.map[i + 1][j] != None):
                     spawn_positions.append(vec(j * TILESIZE, i * TILESIZE))
         return choice(spawn_positions)
+    
+    
+    def save_world_image(self):
+        surf = pg.Surface((self.grid.width, self.grid.height))
+        surf.fill(LIGHTBLUE)
+        for i in range(self.grid.height):
+            for j in range(self.grid.width):
+                if self.grid.map[i][j]:
+                    color = BLOCK_TYPES[self.grid.map[i][j].type]['color']
+                    pg.draw.rect(surf, color, ((j, i), (1, 1)))
+        
+        pg.image.save(surf, 'world.png')
+                
 
     
     def update(self):
         pg.display.set_caption(str(round(self.clock.get_fps(), 2)))
-        
-        self.player.update(self.blocks)
+
+        for sprite in self.all_sprites:
+             if (abs(sprite.pos.x - self.player.pos.x) < 
+                    (SCREEN_WIDTH // 2 + TILESIZE) and
+               (abs(sprite.pos.y - self.player.pos.y)) <
+                    SCREEN_HEIGHT):
+                    # only update what's on the screen
+                    sprite.update()          
         self.camera.update(self.player)
-        
-        for block in self.blocks:
-            block.update()
-        
+
         self.m_pos = vec(pg.mouse.get_pos())
         self.m_pos /= SCREEN_SCALE
         self.m_pos = self.camera.apply_point(self.m_pos)
 
         self.p_to_mouse = self.m_pos - self.player.rect.center
         # scale player_to_mouse vector
-        #if 0 < self.p_to_mouse.length() > 100:
-            #self.p_to_mouse.scale_to_length(100)
+        #if 0 < self.p_to_mouse.length() > self.player.reach:
+        #    self.p_to_mouse.scale_to_length(self.player.reach)
         
         self.block_pos.x = (self.player.rect.centerx + 
                             self.p_to_mouse.x) // TILESIZE * TILESIZE
@@ -585,14 +699,18 @@ class Game:
         
     def draw(self):
         self.screen.blit(self.background, self.camera.apply_rect(self.background_rect))
-
+        
+        # get the rect position of the player on the screen
         rect = self.camera.apply(self.player)
-        self.screen.blit(self.player.image, rect)
         
-        
-        for block in self.blocks:
-            self.screen.blit(block.image, self.camera.apply(block))
-        
+        for sprite in self.all_sprites:
+            if (abs(sprite.pos.x - self.player.pos.x) < (
+                    SCREEN_WIDTH // 2 + TILESIZE) and
+               (abs(sprite.pos.y - self.player.pos.y)) < (
+                    SCREEN_HEIGHT // 2 + TILESIZE)):
+                # only blit what's on the screen
+                self.screen.blit(sprite.image, self.camera.apply(sprite))
+
         # draw a line from player to mouse
         pg.draw.line(self.screen, BLACK, rect.center, 
                      rect.center + self.p_to_mouse, 2)       
@@ -619,7 +737,7 @@ class Game:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.running = False
-            # MEMO: EXPORT THE PRESSED KEYS   !!!
+            # MEMO: EXPORT THE PRESSED KEYS TO A FUNCTION
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
                     self.player.jump(self.blocks)
